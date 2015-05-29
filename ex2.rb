08196e6c2103de11a979c3f30168cee1fa04047d
fargo <peterhjr@gmail.com>
2015-05-29 19:51:38 +0200

    Initial commit

---
+++ ex2.rb
@@ -0,0 +1,8 @@
+# This is apparently an octothorpe character.
+# It's a Ruby comment.
+
+puts "Here's some code doing its thing." # and here's the comment after it.
+
+# Reading code backwards is a good way to catch errors.
+#
+#
---
+++ ex3.rb
@@ -0,0 +1,15 @@
+# Ruby ex3 - numbers and mathematics.
+
+puts "I will now count my chickens:"
+
+puts "Hens: #{25 + 30 /5}"
+puts "Roosters: #{4 + 12 / 8}"
+
+puts "Is 2+5 < 7-2?"
+
+puts 2+5 < 7-2
+
+puts "Is it greater? #{7 > -2}"
+puts "Is it smaller? #{14 < 28}"
+puts "Is it less than or equal to? #{56 <= 7 * 8}"
+
