27eba044bbddfd32aa8356373b9e9e85e685d123
fargo <peterhjr@gmail.com>
2015-05-29 20:43:18 +0200

    added six and seven

---
+++ ex6.rb
@@ -0,0 +1,25 @@
+# Exercise 6: Strings and Text
+#
+
+types_of_people = 10
+x = "There are #{types_of_people} types of people in the wordl."
+binary = "binary"
+do_not = "don't"
+y = "Those who understand #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}"
+puts "And then I said: #{y}"
+
+hilarious = false
+joke_evaluation = "The funniest joke in the wordl is this one. #{hilarious}"
+
+puts joke_evaluation
+
+w = "Left side of the string is followed by "
+e = "the right side of the string."
+
+puts w+e
+
---
+++ ex7.rb
@@ -0,0 +1,15 @@
+# Exercise 7: More Printing
+#
+
+puts "Mary had a little lamb."
+puts "It tasted rather #{'spiffy'}."
+puts "And then she burped and said \"I'll have,"
+puts "Some more in just a jiffy.\""
+
+puts "." * 10
+
+print "It may appear that PRINTing a string"
+puts "will not add a carriage return."
+puts "stringwill!"
+
+
